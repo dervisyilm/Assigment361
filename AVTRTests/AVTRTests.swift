467e32439baca2930de2241fb5f3a8cfb758d172
dervisyilmaz <dyilmaz@gmail.com>
2022-02-10 14:17:22 +0300

    Initial Commit

---
+++ AVTRTests/AVTRTests.swift
@@ -0,0 +1,33 @@
+//
+//  FaceAnimojiTests.swift
+//  FaceAnimojiTests
+//
+//  Created by Dervis YILMAZ on 7.01.2022.
+//
+
+import XCTest
+@testable import FaceAnimoji
+
+class AVTRTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
